82689821d06d21c2b26be0662bbe852a9f4f5b2c
Maxim Malik <maxim.malik@stu.ocsb.ca>
2025-12-16 17:48:45 -0500

    Added program

--- main.ts
+++ main.ts
@@ -1,8 +1,17 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Maxim Malik
+ * Created on: Dec 2025
+ * This program does basic math
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.pause(1000)
+
+basic.showString('A = 3cm x 5cm =' + (3 * 5).toString() + 'cm^2')
+
+basic.clearScreen()
+basic.pause(1000)
+
+basic.showString('P = 2cm(3cm + 5cm) =' + (2 * (3 + 5)).toString() + 'cm')
+basic.showIcon(IconNames.Happy)
